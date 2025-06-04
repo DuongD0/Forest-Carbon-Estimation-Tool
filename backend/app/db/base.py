6f3ede3a11f8c46e7aba548c77f5e7e605256213
DuongD0 <duongdo.workspace@gmail.com>
2025-06-04 13:49:05 -0400

    feat(backend): Add and configure Alembic for database migrations

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,7 +1,7 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from app.db.session import Base  # noqa
-from app.models.user import User, Role, Permission, RolePermission  # noqa
+from app.models.user import User, Role, Permission  # noqa
 # Import other models here as they are created
 # e.g., from app.models.project import Project # noqa
 
